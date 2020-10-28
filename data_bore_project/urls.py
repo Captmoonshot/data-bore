74daf7cbab0b1bf82946e2c3a996131d00a3eee4
Captmoonshot <gygantor@protonmail.com>
2020-10-27 20:04:44 -0700

    attempting to fix collectstatic problems

--- data_bore_project/urls.py
+++ data_bore_project/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
 if settings.DEBUG:
 	urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
-	urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_URL))
+	urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
 
 
