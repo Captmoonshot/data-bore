aabcd39d9ae34cc04d0ac18e6f63d9d6a763e696
Captmoonshot <gygantor@protonmail.com>
2020-10-27 20:51:29 -0700

    updated

--- data_bore_project/settings.py
+++ data_bore_project/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'data_bore_project.urls'
